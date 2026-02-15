253a7d4e9a1cf6e697239e2add84ed2400df9e33
Brian O <brian@example.com>
2026-02-15 12:32:14 +0000

    Force PWA update with SW v2 and cache cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flycabs-v1';
+const CACHE_NAME = 'flycabs-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -8,8 +8,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+            );
+        })
     );
 });
 
